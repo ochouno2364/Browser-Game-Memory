9ec9888b3893305ee1374316c10e2d43323c3437
Ochouno2364 <ochouno2364@gmail.com>
2024-12-16 18:29:09 -0500

    data file

---
+++ data/data.js
@@ -0,0 +1,123 @@
+let ballCards = [
+    {
+        "image": "./images/Ballcards1.png",
+        "name": "Paul Skenes",
+        "back": "./images/hhhhh.png",
+        "alt": "Paul Skenes"
+    },
+    {
+        "image": "./images/Ballcards2.png",
+        "name": "Jazz Chisholm",
+        "back": "./images/hhhhh.png",
+        "alt": "Jazz Chisholm"
+    },
+    {
+        "image": "./images/Ballcards3.png",
+        "name": "Elly De La Cruz",
+        "back": "./images/hhhhh.png",
+        "alt": "Elly De La Cruz"
+
+    },
+    {
+        "image": "./images/Ballcards4.png",
+        "name": "Wander Franco",
+        "back": "./images/hhhhh.png",
+        "alt": "Wander Franco",
+    },
+    {
+        "image": "./images/Ballcards5.png",
+        "name": "Fernando Tatis Jr.",
+        "back": "./images/hhhhh.png",
+        "alt": "Fernando Tatis Jr."
+    },
+    {
+        "image": "./images/Ballcards6.png",
+        "name": "Prelander Berroa",
+        "back": "./images/hhhhh.png",
+        "alt": "Prelander Berroa"
+    },
+    {
+        "image": "./images/Ballcards7.png",
+        "name": "Bo Jackson",
+        "back": "./images/hhhhh.png",
+        "alt": "Bo Jackson"
+    },
+    {
+        "image": "./images/Ballcards8.png",
+        "name": "Ronald Acuna Jr.",
+        "back": "./images/hhhhh.png",
+        "alt": "Ronald Acuna Jr."
+    },
+    {
+        "image": "./images/Ballcards9.png",
+        "name": "Daulton Varsho",
+        "back": "./images/hhhhh.png",
+        "alt": "Daulton Varsho"
+    },
+    {
+        "image": "./images/Ballcards10.png",
+        "name": "Pete Alonso",
+        "back": "./images/hhhhh.png",
+        "alt": "Pete Alonso"
+    },
+    {
+        "image": "./images/Ballcards1.png",
+        "name": "Paul Skenes",
+        "back": "./images/hhhhh.png",
+        "alt": "Paul Skenes"
+    },
+    {
+        "image": "./images/Ballcards2.png",
+        "name": "Jazz Chisholm",
+        "back": "./images/hhhhh.png",
+        "alt": "Jazz Chisholm"
+    },
+    {
+        "image": "./images/Ballcards3.png",
+        "name": "Elly De La Cruz",
+        "back": "./images/hhhhh.png",
+        "alt": "Elly De La Cruz"
+    },
+    {
+        "image": "./images/Ballcards4.png",
+        "name": "Wander Franco",
+        "back": "./images/hhhhh.png",
+        "alt": "Wander Franco"
+        
+    },
+    {
+        "image": "./images/Ballcards5.png",
+        "name": "Fernando Tatis Jr.",
+        "back": "./images/hhhhh.png",
+        "alt": "Fernando Tatis Jr."
+    },
+    {
+        "image": "./images/Ballcards6.png",
+        "name": "Prelander Berroa",
+        "back": "./images/hhhhh.png"
+    },
+    {
+        "image": "./images/Ballcards7.png",
+        "name": "Bo Jackson",
+        "back": "./images/hhhhh.png",
+        "alt": "Bo Jackson"
+    },
+    {
+        "image": "./images/Ballcards8.png",
+        "name": "Ronald Acuna Jr.",
+        "back": "./images/hhhhh.png",
+        "alt": "Ronald Acuna Jr."
+    },
+    {
+        "image": "./images/Ballcards9.png",
+        "name": "Daulton Varsho",
+        "back": "./images/hhhhh.png",
+        "alt": "Daulton Varsho"
+    },
+    {
+        "image": "./images/Ballcards10.png",
+        "name": "Pete Alonso",
+        "back": "./images/hhhhh.png",
+        "alt": "Pete Alonso"
+    }
+];
